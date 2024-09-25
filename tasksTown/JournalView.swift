5bd4f4687ea7d2d997b73f99d56779ec6b80295e
BradsTheDeveloper <bradleyaustin2611@gmail.com>
2024-09-25 17:39:27 +0100

    Created Spotify, Time, Tasks widgets - working

---
+++ tasksTown/JournalView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalView.swift
+//  tasksTown
+//
+//  Created by Bradley Austin on 25/09/2024.
+//
+
+import SwiftUI
+
+struct JournalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalView()
+}
---
+++ tasksTown/Widget.swift
@@ -0,0 +1,18 @@
+//
+//  Widget.swift
+//  tasksTown
+//
+//  Created by Bradley Austin on 25/09/2024.
+//
+
+import SwiftUI
+
+struct Widget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Widget()
+}
